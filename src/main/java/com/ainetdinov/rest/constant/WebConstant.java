dfc2d69c3983272f04cee57066eabe47509d35e0
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-07-13 16:53:16 +0300

    Listener directory

---
+++ src/main/java/com/ainetdinov/rest/constant/WebConstant.java
@@ -0,0 +1,27 @@
+package com.ainetdinov.rest.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class WebConstant {
+    public static final String STUDENT_SERVICE = "studentService";
+    public static final String TEACHER_SERVICE = "teacherService";
+    public static final String GROUP_SERVICE = "groupService";
+    public static final String SCHEDULE_SERVICE = "scheduleService";
+    public static final String HTTP_SERVICE = "httpService";
+    public static final String PARSER_SERVICE = "parserService";
+
+    public static final String NUMBER = "number";
+    public static final String SURNAME = "surname";
+    public static final String SUBJECTS = "subjects";
+    public static final String GROUP_NUMBER = "groupNumber";
+    public static final String STUDENT_NAME = "studentName";
+    public static final String TEACHER_NAME = "teacherName";
+    public static final String STUDENT_SURNAME = "studentSurname";
+    public static final String SCHEDULE_DATE = "date";
+
+    public static final String STUDENTS_PATH = "students.path";
+    public static final String TEACHERS_PATH = "teacher.path";
+    public static final String GROUPS_PATH = "groups.path";
+    public static final String SCHEDULES_PATH = "schedules.path";
+}
---
+++ src/main/java/com/ainetdinov/rest/listener/AppContextListener.java
@@ -0,0 +1,71 @@
+package com.ainetdinov.rest.listener;
+
+import com.ainetdinov.rest.constant.WebConstant;
+import com.ainetdinov.rest.model.Group;
+import com.ainetdinov.rest.model.Schedule;
+import com.ainetdinov.rest.model.Student;
+import com.ainetdinov.rest.model.Teacher;
+import com.ainetdinov.rest.service.*;
+import com.ainetdinov.rest.validator.GroupValidator;
+import com.ainetdinov.rest.validator.ScheduleValidator;
+import com.ainetdinov.rest.validator.StudentValidator;
+import com.ainetdinov.rest.validator.TeacherValidator;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Properties;
+
+@WebListener
+public class AppContextListener implements ServletContextListener {
+    private static final String PROPERTIES_PATH = "/WEB-INF/resources/settings.properties";
+    private final Properties properties = new Properties();
+    private final ParsingService parsingService = new ParsingService();
+    private ServletContext context;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        context = sce.getServletContext();
+        initProperties();
+
+        StudentService studentService = new StudentService(initEntities(WebConstant.STUDENTS_PATH, Student.class), new StudentValidator());
+        TeacherService teacherService = new TeacherService(initEntities(WebConstant.TEACHERS_PATH, Teacher.class), new TeacherValidator());
+        GroupService groupService = new GroupService(initEntities(WebConstant.GROUPS_PATH, Group.class), new GroupValidator(), studentService);
+        ScheduleService scheduleService = new ScheduleService(initEntities(WebConstant.SCHEDULES_PATH, Schedule.class), new ScheduleValidator(), teacherService, groupService);
+
+        context.setAttribute(WebConstant.PARSER_SERVICE, parsingService);
+        context.setAttribute(WebConstant.STUDENT_SERVICE, studentService);
+        context.setAttribute(WebConstant.TEACHER_SERVICE, teacherService);
+        context.setAttribute(WebConstant.GROUP_SERVICE, groupService);
+        context.setAttribute(WebConstant.SCHEDULE_SERVICE, scheduleService);
+        context.setAttribute(WebConstant.HTTP_SERVICE, new HttpService());
+
+        ServletContextListener.super.contextInitialized(sce);
+    }
+
+    private void initProperties() {
+        try {
+            properties.load(context.getResourceAsStream(PROPERTIES_PATH));
+        } catch (IOException e) {
+            throw new IllegalStateException(String.format("Error reading properties file: %s", PROPERTIES_PATH), e);
+        }
+    }
+
+    private Path getResourcePath(String path) {
+        try {
+            return Path.of(context.getResource(properties.getProperty(path)).toURI());
+        } catch (URISyntaxException | MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private <T> List<T> initEntities(String path, Class<T> clazz) {
+        return parsingService.parseList(getResourcePath(path), clazz);
+    }
+}
