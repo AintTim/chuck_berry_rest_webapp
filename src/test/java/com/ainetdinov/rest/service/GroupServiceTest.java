23a6f42057bfe4c20d1a002bcb8d810dfbe127c5
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-07-16 21:25:31 +0300

    Testing

---
+++ src/test/java/com/ainetdinov/rest/service/GroupServiceTest.java
@@ -0,0 +1,44 @@
+package com.ainetdinov.rest.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class GroupServiceTest {
+
+    @Test
+    void getEntities() {
+
+    }
+
+    @Test
+    void getEntity() {
+    }
+
+    @Test
+    void testGetEntity() {
+    }
+
+    @Test
+    void testGetEntities() {
+    }
+
+    @Test
+    void addGroup() {
+    }
+
+    @Test
+    void addStudentsToGroup() {
+    }
+
+    @Test
+    void getGroupByStudentNameAndSurname() {
+    }
+
+    @Test
+    void getStudentService() {
+    }
+}
