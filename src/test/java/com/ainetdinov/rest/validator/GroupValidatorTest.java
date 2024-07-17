c17ae1b5f835b43ba9ef3e524db48700b1ec2bb4
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-07-17 19:47:43 +0300

    GroupValidatorTest

---
+++ src/test/java/com/ainetdinov/rest/validator/GroupValidatorTest.java
@@ -0,0 +1,46 @@
+package com.ainetdinov.rest.validator;
+
+import com.ainetdinov.rest.model.Group;
+import com.ainetdinov.rest.service.BaseServiceTest;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class GroupValidatorTest extends BaseServiceTest {
+    private final GroupValidator groupValidator = new GroupValidator();
+
+    @Test
+    void validate_ShouldReturnTrue_WhenValidGroup() {
+        assertThat(groupValidator.validate(defaultGroup()), Matchers.is(true));
+    }
+
+    @Test
+    void validate_ShouldReturnTrue_WhenNullUuid() {
+        Group group = defaultGroup();
+        defaultGroup().setUuid(null);
+        assertThat(groupValidator.validate(group), Matchers.is(true));
+    }
+
+    @Test
+    void validate_ShouldReturnFalse_WhenGroupIsNull() {
+        assertThat(groupValidator.validate(null), Matchers.is(false));
+    }
+
+    @Test
+    void validate_ShouldReturnFalse_WhenPhoneContainsLetters() {
+        Group group = defaultGroup();
+        group.setNumber(group.getNumber() + "abc");
+        assertThat(groupValidator.validate(group), Matchers.is(false));
+    }
+
+    @Test
+    void validate_ShouldReturnFalse_WhenInvalidUuid() {
+        Group group = defaultGroup();
+        group.setUuid(group.getUuid() + "abc");
+        assertThat(groupValidator.validate(group), Matchers.is(false));
+    }
+}
